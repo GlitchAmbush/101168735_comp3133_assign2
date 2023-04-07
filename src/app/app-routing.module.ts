005d007fcd0d4bb7f5dc44781c685b60bc5b594a
GlitchAmbush <lemmytheleo@yahoo.com>
2023-04-07 18:51:54 -0400

    Front-End Styling implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { MainComponent } from './main/main.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { ViewEmployeeComponent } from './view-employee/view-employee.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'login', component: LoginComponent },
   { path: 'sign-up', component: SignupComponent },
   { path: 'add-employee/_add', component: AddEmployeeComponent },
+  { path: 'view-employee', component: ViewEmployeeComponent}
 ];
 
 @NgModule({
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/index.html',
+    './src/app/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
